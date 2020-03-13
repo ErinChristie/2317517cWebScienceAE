dc82c56a89ef0d8cfb8f638123ce8d52c7d21797
ErinChristie <2317517c@student.gla.ac.uk>
2020-03-13 13:43:24 +0000

    updated TwitterStream.py

--- TwitterStream.py
+++ TwitterStream.py
@@ -12,8 +12,7 @@ connection = Connection('localhost', 27017)
 # Connect to MongoDB database 
 db = connection.webscienceae
 db.myCollection.ensure_index("id", unique=True, dropDups=True)
-db.RESTCollection.ensure_index("id", unique=True, dropDups=True)
-collection = db.myCollection
+collection = db.RESTCollection
 
 # Define keywords to use and ensure language is English
 keywords = ['megxit', 'meghan', 'harry']
@@ -28,8 +27,7 @@ access_secret = ""
 class StdOutListener(StreamListener):
 
     def on_data(self, data):
-
-        # Parse Tweet to variable Load the Tweet into the variable "t"
+        # Parse Tweet to variable tweet 
         tweet = json.loads(data)
         try: 
             # Try to store data collected in database 
@@ -43,15 +41,52 @@ class StdOutListener(StreamListener):
 
             # Create a date object using the created_at timestamp so it can be used by MongoDB
             created = datetime.datetime.strptime(created_at, '%a %b %d %H:%M:%S +0000 %Y')
+            
+            # Initialise the REST API
+            # Write to file the followers of each user
+            # Write the followers of each user to the file "followers.txt"
+            list_of_followers = []
 
+            file = open("followers.txt","a+")
+            current_cursor = tweepy.Cursor(api.followers_ids, screen_name=username, count=10)
+            current_followers = current_cursor.iterator.next()
+            list_of_followers.extend(current_followers)
+            next_cursor_id = current_cursor.iterator.next_cursor
+
+            while(next_cursor_id!=0):
+                current_cursor = tweepy.Cursor(self.api.followers_ids, screen_name=username, count=10,cursor=next_cursor_id)
+                current_followers=current_cursor.iterator.next()
+                list_of_followers.extend(current_followers)
+                next_cursor_id = current_cursor.iterator.next_cursor
+               
+            file.write(list_of_followers)
+            file.close()  
             # Put all data collected for a single Tweet into a variable
             tweet_data = {'id':tweet_id, 'username':username, 'followers':followers, 'text':text, 'hashtags':hashtags, 'language':language, 'created':created}
 
             # Store the varibale tweet_data into the database 
             collection.save(tweet_data)
 
+             # Get the full list of followers of a particular user
+            list_of_followers=[]
+
+            file = open("user_followers.txt","w")
+            current_cursor = tweepy.Cursor(api.followers_ids, screen_name="DailyMirror", count=5000)
+            current_followers = current_cursor.iterator.next()
+            list_of_followers.extend(current_followers)
+            next_cursor_id = current_cursor.iterator.next_cursor
+
+            while(next_cursor_id!=0):
+                current_cursor = tweepy.Cursor(self.api.followers_ids, screen_name="DailyMirror", count=5000,cursor=next_cursor_id)
+                current_followers=current_cursor.iterator.next()
+                list_of_followers.extend(current_followers)
+                next_cursor_id = current_cursor.iterator.next_cursor
+                #file.write(list_of_followers)
+
         except:
             print(tweet)
+
+        file.write(list_of_followers)
 
     # Prints error status if an error occurs 
     def on_error(self, status):
